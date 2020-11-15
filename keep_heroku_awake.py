b6497eefc4ceb84f9c1c766d8d141b64b2becef8
ben4932042 <63529371+ben4932042@users.noreply.github.com>
2020-11-16 02:59:20 +0800

    restructure ping Heroku script

--- keep_heroku_awake.py
+++ keep_heroku_awake.py
@@ -1,9 +1,22 @@
-import requests
+import os
 import time
+import logging
+import requests
+from apscheduler.schedulers.blocking import BlockingScheduler
 
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) '
-                         'Chrome/80.0.3987.132 Safari/537.36'}
-while 1:
-    res = requests.get('https://oolala.herokuapp.com', headers=headers)
-    print(res.text)
-    time.sleep(60)
+HEROKU_URL="https://oolala.herokuapp.com"
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('cron', minute="*")
+def keep_heroku_awake():
+    res = requests.get(HEROKU_URL)
+    if res.status_code != 200:
+        logging.ERROR("heroku is dead")
+    else:
+        pass
+
+if __name__ == "__main__":
+    logging.getLogger('apscheduler')\
+           .setLevel(logging.DEBUG)
+    sched.start()
